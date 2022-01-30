721d8aaf9367d1e75ddd1058b0b58ca058d871e1
drizzleEnt <70242740+drizzleEnt@users.noreply.github.com>
2022-01-30 22:30:33 +0300

    start adding view

--- Assets/Source/Model/LazerGunRollback.cs
+++ Assets/Source/Model/LazerGunRollback.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LazerGunRollback 
 {
@@ -8,14 +9,25 @@ public class LazerGunRollback
 
     private float _elapsedTime = 0;
 
+    public event UnityAction OnStopRollingBack;
+
     public bool CanShoot => _elapsedTime <= 0;
+    public float ElapsedTime => _elapsedTime;
 
     public void Update(float deltaTime)
     {
         if (_elapsedTime <= 0)
             return;
 
+        DecreaseTime(deltaTime);
+    }
+
+    private void DecreaseTime(float deltaTime)
+    {
         _elapsedTime -= deltaTime;
+
+        if (_elapsedTime <= 0)
+            OnStopRollingBack?.Invoke();
     }
 
     public void SetShoted()
